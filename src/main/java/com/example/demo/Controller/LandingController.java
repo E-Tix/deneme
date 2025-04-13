635b4d10b04b786b24c7eb1cc490066061abcd58
beratavseren <musaberatavseren@gmail.com>
2025-04-14 02:06:50 +0300

    LandingController düzenlendi

--- src/main/java/com/example/demo/Controller/LandingController.java
+++ src/main/java/com/example/demo/Controller/LandingController.java
@@ -1,14 +1,33 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Entity.EtkinlikEntity;
+import com.example.demo.Service.LandingService;
 import jakarta.persistence.Entity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/user")
-public class LandingController {
+import java.util.List;
 
-    public boolean deneme(){return true;}
+@RestController
+@RequestMapping("/landing")
+public class LandingController {
+    private LandingService landingService;
+
+    @Autowired
+    public LandingController(LandingService landingService)
+    {
+        this.landingService=landingService;
+    }
+
+    @GetMapping("/")
+    public List<EtkinlikEntity> getEtkinlik(
+            @RequestParam(required = false) String etkinlikTurAdi,
+            @RequestParam(required = false) String sehirAdi
+    ){
+        return landingService.getEtkinlik(etkinlikTurAdi,sehirAdi);
+    }
 
 }
